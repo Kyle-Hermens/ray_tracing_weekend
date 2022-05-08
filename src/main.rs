3cff263f9cd90fe90591346691a5f89851ce782c
Kyle_Hermens <kyle.hermens@gmail.com>
2022-05-08 12:06:25 -0600

    Step 6.3

--- src/main.rs
+++ src/main.rs
@@ -270,3 +270,50 @@ impl Ray {
             }
     }
 }
+
+struct HitRecord {
+    p: Point3,
+    normal: Vec3,
+    t: f64,
+}
+
+trait Hittable {
+    fn hit(&self, r: Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        false
+    }
+}
+
+struct Sphere {
+    center: Point3,
+    radius: f64,
+}
+
+impl Hittable for Sphere {
+    fn hit(&self, r: Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let oc = r.origin - self.center;
+        let a = r.direction.length_squared();
+        let half_b = Vec3::dot(oc, r.direction);
+        let c = oc.length_squared() - self.radius * self.radius;
+
+        let discriminant = half_b * half_b - a * c;
+        if discriminant < 0.0 {
+            return false;
+        };
+
+        let sqrtd = discriminant.sqrt();
+
+        let mut root = (-half_b - sqrtd) / a;
+        if root < t_min || t_max < root {
+            root = (-half_b + sqrtd) / a;
+            if root < t_min || t_max < root {
+                return false;
+            }
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        rec.normal = (rec.p - self.center) / self.radius;
+
+        return true;
+    }
+}
